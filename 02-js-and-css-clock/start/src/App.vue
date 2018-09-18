7c0a508fe31e0ea63d9ed84e2787917d1d15c2f9
Jason Yu <me@ycmjason.com>
2018-09-18 13:29:20 +0100

    02: added start

---
+++ 02-js-and-css-clock/start/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Clock />
+  </div>
+</template>
+
+<script>
+import Clock from './components/Clock.vue'
+
+export default {
+  name: 'app',
+  components: { Clock },
+}
+</script>
+
+<style>
+html {
+  background:#018DED url(http://unsplash.it/1500/1000?image=881&blur=50);
+  background-size:cover;
+  font-family:'helvetica neue';
+  text-align: center;
+  font-size: 10px;
+}
+
+#app {
+  margin: 0;
+  font-size: 2rem;
+  display:flex;
+  flex:1;
+  min-height: 100vh;
+  align-items: center;
+}
+</style>
