f6adf6649016219bd0fef2c12a6026ffaea08058
Jason Yu <me@ycmjason.com>
2018-10-09 10:32:18 +0100

    03 start

---
+++ 03-css-variables/start/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <h2>Update CSS Variables with <span class='hl'>VUE.JS</span></h2>
+
+    <div class="controls">
+      <label>
+        Spacing:
+        <input type="range" min="10" max="200" value="10">
+      </label>
+
+      <label>Blur:
+        <input type="range" min="0" max="25" value="10">
+      </label>
+
+      <label>
+        Base Color:
+        <input type="color" value="#ffc600">
+      </label>
+    </div>
+
+    <img src="./assets/picture.jpg">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+}
+</script>
+
+<style scoped>
+img {
+  padding: var(--spacing);
+  background: var(--base);
+  filter: blur(var(--blur));
+}
+
+.hl {
+  color: var(--base);
+}
+</style>
