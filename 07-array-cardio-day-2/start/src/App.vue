303c1252d39d2469f78bff732b75b6378215a280
Jason Yu <me@ycmjason.com>
2018-11-20 10:45:31 +0000

    added start 07

---
+++ 07-array-cardio-day-2/start/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app" v-if="people && comments">
+    <h1>Array Cardio</h1>
+    
+    <h2>Is at least one person 19 or older?</h2>
+    Answer here
+
+    <h2>Is everyone 19 or older?</h2>
+    Answer here
+
+    <h2>Find the comment with the ID of 823423</h2>
+    Answer here
+  </div>
+</template>
+
+<script>
+const getAgeFromBirthYear = birthYear => (new Date()).getFullYear() - birthYear;
+
+export default {
+  name: 'app',
+};
+</script>
+
+<style>
+</style>
